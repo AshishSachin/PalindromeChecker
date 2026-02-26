0cb50876da8ccf948de5fd30397fa1aa6feab602
AshishSachin <amarsachin07@gmail.com>
2026-02-26 08:36:18 +0530

    UC4

---
+++ palindromechecker/src/PalindromeChecker.java
@@ -0,0 +1,34 @@
+public class PalindromeChecker {
+
+
+    public static void main(String[] args){
+        /*
+        UC4
+         */
+        String input = "madam";
+
+
+        char[] characters = input.toCharArray();
+
+
+        int start = 0;
+        int end = characters.length - 1;
+
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (characters[start] != characters[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
+
+
+    }
+}
